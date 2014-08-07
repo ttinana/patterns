db35acb16ab9311cd1b30fb4bc1c202741730dee
kapyar <123kapyar@gmail.com>
2014-08-07 19:00:17 +0300

    Bridge Proxy COR

---
+++ Proxy/Proxy/src/IRobot.java
@@ -0,0 +1,13 @@
+public interface IRobot {
+
+	public boolean isConnected();
+
+	public void walkStreight(int steps);
+
+	public void turnLeft();
+
+	public void turnRight();
+
+	public void defuseBomb();
+
+}
